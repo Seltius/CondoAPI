7c265350b580071fd5bcbc43ab3295a2c720cb43
José João Dantas <jose_dantas_80@hotmail.com>
2023-11-09 15:50:53 +0000

    Fix handleException mechanism bug

--- src/main/java/pt/iscte/condo/config/GlobalExceptionHandler.java
+++ src/main/java/pt/iscte/condo/config/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package pt.iscte.condo.config;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pt.iscte.condo.exceptions.BusinessException;
 
@@ -17,8 +20,9 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    private ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                           HttpStatusCode status, WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
 
         List<String> errors = ex.getBindingResult()
