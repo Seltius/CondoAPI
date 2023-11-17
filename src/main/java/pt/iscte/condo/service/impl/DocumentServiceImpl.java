c0109736a473d00ef603c6c0f6f5cae8db3720e5
José João Dantas <jose_dantas_80@hotmail.com>
2023-11-17 14:14:36 +0000

    Simplify service layer main methods

--- src/main/java/pt/iscte/condo/service/impl/DocumentServiceImpl.java
+++ src/main/java/pt/iscte/condo/service/impl/DocumentServiceImpl.java
@@ -11,7 +11,7 @@ import pt.iscte.condo.controller.response.DocumentResponse;
 import pt.iscte.condo.controller.response.FileResponse;
 import pt.iscte.condo.domain.Document;
 import pt.iscte.condo.domain.User;
-import pt.iscte.condo.exceptions.BusinessException;
+import pt.iscte.condo.enums.Role;
 import pt.iscte.condo.mapper.DocumentMapper;
 import pt.iscte.condo.repository.DocumentRepository;
 import pt.iscte.condo.repository.UserRepository;
@@ -33,34 +33,21 @@ public class DocumentServiceImpl implements DocumentService {
 
     @Override
     public void uploadDocument(DocumentRequest request) {
-        String fileData = request.getFileData();
-
-        if (!isPdfFile(fileData)) {
-            throw new BusinessException("fileType not allowed (only pdf)");
-        }
-
-        User owner =  userRepository.findById(request.getOwnerId())
-                .orElseThrow(() -> new BusinessException("User does not exist")); //TODO NOT WORKING (FIX ME)
-
-        User uploader = getUserFromBearer(getBearer(httpRequest));
-
-        Document document = documentMapper.documentRequestToDocument(request);
-
-        document.setOwner(owner);
-        document.setUploader(uploader);
-        document.setUploadDate(LocalDateTime.now());
-
-        documentRepository.save(document);
-
+        User uploader = getUserByBearer(getBearer(httpRequest));
+        validateAdminRole(uploader);
+        validateFile(request.getFileData());
+        storeDocument(request, uploader);
     }
 
     @Override
     public FileResponse getDocument(Integer id) {
 
-        //TODO ONLY ALLOW DOWNLOAD IF IT'S FROM THE OWNER OR UPLOADER
-
         Document document = documentRepository.findById(id)
-                .orElseThrow(() -> new BusinessException("Document not found"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Document not found"));
+
+        User user = getUserByBearer(getBearer(httpRequest));
+        if (!user.getId().equals(document.getOwner().getId()))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access Denied");
 
         return documentMapper.documentToDocumentResponse(document);
     }
@@ -68,7 +55,7 @@ public class DocumentServiceImpl implements DocumentService {
     @Override
     public List<DocumentResponse> getDocuments() {
 
-        User owner = getUserFromBearer(getBearer(httpRequest));
+        User owner = getUserByBearer(getBearer(httpRequest));
 
         List<Document> documentList = documentRepository.findAllByOwnerId(owner.getId())
                 .orElseThrow(() -> new RuntimeException("No documents found"));
@@ -82,23 +69,46 @@ public class DocumentServiceImpl implements DocumentService {
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             return authHeader.substring(7);
-            // Now you have the bearer token, you can use it as needed
         } else {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access Denied");
         }
 
     }
 
-    private User getUserFromBearer(String token) {
-        return userRepository.findByEmail(jwtService.getUsername(token))
-                .orElseThrow(() -> new BusinessException("User does not exist"));
+    private void storeDocument(DocumentRequest request, User uploader) {
+        User owner =  userRepository.findById(request.getOwnerId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        Document document = documentMapper.documentRequestToDocument(request);
+        document.setOwner(owner);
+        document.setUploader(uploader);
+        document.setUploadDate(LocalDateTime.now());
+
+        documentRepository.save(document);
+
     }
 
-    private boolean isPdfFile(String base64File) {
+    private void validateAdminRole(User uploader) {
+        if (!uploader.getRole().equals(Role.ADMIN))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access Denied");
+    }
+
+    private User getUserByBearer(String token) {
+        return userRepository.findByEmail(jwtService.getUsername(token))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    private void validateFile(String base64File) {
         byte[] decodedBytes = Base64.getDecoder().decode(base64File);
         Tika tika = new Tika();
         String fileType = tika.detect(decodedBytes);
-        return fileType.equals("application/pdf");
+
+        if (!fileType.equals("application/pdf"))
+            throw new ResponseStatusException(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "file type not supported");
+
+        if(decodedBytes.length > 2000000)
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "file size too big");
+
     }
 
 
