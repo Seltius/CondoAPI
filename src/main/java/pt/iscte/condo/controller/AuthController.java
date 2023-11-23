44623ce1a3029132183bcbda36918410cb95c51e
José João Dantas <jose_dantas_80@hotmail.com>
2023-11-23 18:36:55 +0000

    Auth added

--- src/main/java/pt/iscte/condo/controller/AuthController.java
+++ src/main/java/pt/iscte/condo/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
-    @GetMapping("/validate") //TODO ENDPOINT REQUER AUTENTICAÇÃO
+    @GetMapping("/validate")
     public ResponseEntity<ValidateTokenResponse> validate(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(authService.validate(token));
     }
