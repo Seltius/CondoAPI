ffe560a2212430109d483100701c8bf9f3933484
José João Dantas <jose_dantas_80@hotmail.com>
2024-01-01 17:22:02 +0000

    fix CondominiumUserRepository

--- src/main/java/pt/iscte/condo/repository/CondominiumUserRepository.java
+++ src/main/java/pt/iscte/condo/repository/CondominiumUserRepository.java
@@ -1,8 +1,9 @@
 package pt.iscte.condo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pt.iscte.condo.domain.CondominiumUser;
 
-public interface CondominiumUserRepository extends JpaRepository<CondominiumUserRepository, Integer> {
+public interface CondominiumUserRepository extends JpaRepository<CondominiumUser, Integer> {
 
 
 }
